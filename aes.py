7de633a7841d0736aa4956f3ede1d076ce6d1a36
miaoyc <miaoyongchao@qianxin.com>
2022-09-17 16:14:16 +0800

    ====feat: update aes====

--- aes.py
+++ aes.py
@@ -1,4 +1,5 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
 
 """
 File name    : aes.py
@@ -7,19 +8,14 @@ Create date  : 2022/9/16 15:02
 Description  : aes加解密示例
 """
 
+import base64
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
+
 """
 需要安装python包如下
 pip install pycryptodome
 """
-
-import base64
-
-from Crypto.Cipher import AES
-from Crypto.Util import Padding
-from Crypto.Util.py3compat import bchr
-
-AES_SECRET_KEY = "miaoyc@@aes@@key"
-IV = "1234567890123456"
 
 
 class AesSecurity(object):
@@ -27,22 +23,21 @@ class AesSecurity(object):
     Aes加密类，定义常用加解密方法
     """
 
-    block_size = 16
-
     def __init__(self):
-        pass
+        self.block_size = 16
 
     def __pad(self, text):
-        """填充方式，加密内容必须为16字节的倍数，若不足则使用self.iv进行填充"""
+        """
+        加密key填充算法
+        填充方式，加密内容必须为16字节的倍数，若不足则使用0进行填充
+        :param text:
+        :return:
+        """
         amount_to_pad = self.block_size - (len(text) % self.block_size)
-        pad = bchr(0)
+        pad = str.encode("0")
         return text.encode("utf-8") + pad * amount_to_pad
 
-    def __unpad(self, text):
-        pad = ord(text[-1])
-        return text[:-pad]
-
-    def getAES(self, key, data, mode=AES.MODE_ECB, style='pkcs7'):
+    def encrypt(self, key, data, mode=AES.MODE_ECB, style='pkcs7'):
         """AES加密
         :param key: 秘钥key
         :param data: 未加密数据
@@ -54,41 +49,31 @@ class AesSecurity(object):
             :var MODE_CTR: :ref:`CounTer Mode (CTR) <ctr_mode>`
             :var MODE_OPENPGP:  :ref:`OpenPGP Mode <openpgp_mode>`
             :var MODE_EAX: :ref:`EAX Mode <eax_mode>`
-
-        #这里密钥key 长度必须为16（AES-128）、24（AES-192）、或32（AES-256）Bytes 长度.目前AES-128足够用
-
-        :param block_size: 填充block大小：默认为8
         :param style: 填充算法：‘pkcs7’(default),‘iso7816’or‘x923’
         :return: 加密结果 byte string
         """
-
+        # AES.block_size = 16, 使用16位填充
         _data = Padding.pad(data.encode('utf-8'), block_size=AES.block_size, style=style)
+        # 这里密钥key 长度必须为16（AES-128）、24（AES-192）、或32（AES-256）Bytes 长度.目前AES-128足够用
         _key = self.__pad(text=key)
         cipher = AES.new(_key, mode=mode)
         return base64.b64encode(cipher.encrypt(_data)).decode("utf-8")
 
-    def decodeAES(self, key, data, mode=AES.MODE_ECB, style="pkcs7"):
-        entrydata = base64.b64decode(data.encode("utf-8"))
-        _data = Padding.pad(entrydata, block_size=AES.block_size, style=style)
+    def decrypt(self, key, data, mode=AES.MODE_ECB, style="pkcs7"):
+        entry_data = base64.b64decode(data.encode("utf-8"))
+        _data = Padding.pad(entry_data, block_size=AES.block_size, style=style)
         _key = self.__pad(text=key)
         cipher = AES.new(_key, mode=mode)
-        return Padding.unpad(cipher.decrypt(_data)[:int(len(cipher.decrypt(_data)) / 16 - 1) * 16],
-                             AES.block_size).decode("utf-8")
+        return Padding.unpad(
+            cipher.decrypt(_data)[:int(len(cipher.decrypt(_data)) / 16 - 1) * 16], AES.block_size).decode("utf-8")
 
 
 if __name__ == '__main__':
     security = AesSecurity()
-    security.block_size = 32
-    entrydata = security.getAES(key="1111111", data="86-123456789")
-    print(entrydata)
-    detrydata = security.decodeAES(key="1111111", data=entrydata)
-    print(detrydata)
-
-    security.block_size = 16
-    entrydata = security.getAES(key="1111111", data="86-123456789")
-    print(entrydata)
-    detrydata = security.decodeAES(key="1111111", data=entrydata)
-    print(detrydata)
+    a = security.encrypt(key="miaoyc-key", data="miaoyc")
+    print(a)
+    b = security.decrypt(key="miaoyc-key", data=a)
+    print(b)
 
 
 

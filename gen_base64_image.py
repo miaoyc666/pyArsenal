660867bec01d662fc258dd798f709b7d810f5411
miaoyc <miaoyongchao@qianxin.com>
2022-09-17 16:23:29 +0800

    ====doc: update====

--- gen_base64_image.py
+++ gen_base64_image.py
@@ -14,10 +14,10 @@ def convert_2_base64(file_path):
 
 def run():
     if len(sys.argv) != 2:
-        print "example: python gen_base64_image.py file_path"
+        print("example: python gen_base64_image.py file_path")
         sys.exit(1)
     file_path = sys.argv[1]
-    print convert_2_base64(file_path)
+    print(convert_2_base64(file_path))
 
 
 if __name__ == '__main__':

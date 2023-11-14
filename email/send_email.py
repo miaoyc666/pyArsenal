85e3665c5abd5bcc83f1f43a099515c2b5eb3b2b
miaoyc <miaoyc1989@hotmail.com>
2023-11-14 16:39:59 +0800

    ====feat: add send email====

---
+++ email/send_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : send_email.py
+Author       : miaoyc
+Create Date  : 2023/11/14 16:39
+Update Date  : 2023/11/14 16:39
+Description  : 发送电子邮件
+"""

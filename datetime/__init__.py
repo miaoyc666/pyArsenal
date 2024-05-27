48342d564777ea879e95a86365e74966564132db
miaoyc <miaoyc666@outlook.com>
2024-05-27 16:01:32 +0800

    feat(datetime): init

---
+++ datetime/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : __init__.py
+Create time  : 2024/5/27 16:00
+Update time  : 2024/5/27 16:00
+Description  :
+"""
---
+++ datetime/date.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : date.py
+Create time  : 2024/5/27 16:00
+Update time  : 2024/5/27 16:00
+Description  : 
+"""
